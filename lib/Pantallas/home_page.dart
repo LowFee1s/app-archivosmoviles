7aa7cd411f530aaaed34579c79c18074f3644731
Jav1er <javier12bg@gmail.com>
2024-03-09 19:22:23 -0600

    Import y modificaciones para la api de google drive agregando scopes

--- lib/Pantallas/home_page.dart
+++ lib/Pantallas/home_page.dart
@@ -4,6 +4,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:appmovilesproyecto17/Apis/cloud_servicios.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -16,6 +18,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   User? user = FirebaseAuth.instance.currentUser;
+  CloudServicios cloudServicios = new CloudServicios();
+
+  final googleSignIn = GoogleSignIn.standard(
+    scopes: [
+      'https://www.googleapis.com/auth/drive.file',
+      'https://www.googleapis.com/auth/drive',
+    ],
+  );
+
 
   @override
   void initState() {
@@ -46,12 +57,47 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, Constantes.ArchivosNavegacion);
+                },
+                child: Text('Ver archivos')
+            ),
             Text(user!.email!),
             Text(user!.displayName!),
             CircleAvatar(
               backgroundImage: NetworkImage(user!.photoURL!),
               radius: 20,
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Implementar conexión/desconexión con OneDrive
+                  },
+                  child: Text('OneDrive'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Implementar conexión/desconexión con Dropbox
+                  },
+                  child: Text('Dropbox'),
+                ),
+                ElevatedButton(
+                  onPressed: user!.providerData[0].providerId == "google.com" || cloudServicios.isConectadoGoogleDrive ? null
+                  : () async {
+                      if (cloudServicios.isConectadoGoogleDrive) {
+                        await cloudServicios.disconnectGoogleDrive();
+                      } else {
+                        await cloudServicios.connectGoogleDrive();
+                      }
+                  } ,
+                  child: Text('Google Drive'),
+                ),
+              ],
+            ),
+
           ],
         ),
       ),
